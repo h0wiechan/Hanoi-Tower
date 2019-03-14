09d86bad10423e10f6e9e675eb787db79b17e232
h0wiechan <howiechanhy@outlook.com>
2019-03-14 15:31:19 -0400

    Enabled generating discs in different colors

--- src/components/tower.js
+++ src/components/tower.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import { createArrayOfLength } from '../util/general_util';
 class Tower extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
         loaded: false,
     }
-    this.style = {
-      background: '#666',
-      height: '80%',
+    this.colors = {
+      0: '#E02936', // red
+      1: '#D6752D', // orange
+      2: '#BAB341', // yellow
+      3: '#189A79', // green
+      4: '#107392', // blue
+      5: '#493362', // purple
+      6: '#6A5279', // light purple
+      7: '#5C6D98', // greyish blue
     }
   }
 
@@ -16,11 +22,22 @@ class Tower extends React.Component {
     setTimeout(() => this.setState({loaded: true}), this.props.delay);
   }
 
+  generateDiscs() {
+    return (
+      createArrayOfLength(this.props.noOfDiscs).map((i) => {
+        const style = {
+          background: this.colors[i],
+          transform: `scaleX(${2.0 + 1 * i})`,
+        }
+        return <li key={i} className="disc" style={style}></li>
+      })
+    )
+  }
+
   render() {
     return (
       <ul id={`tower-${this.props.idx}`} className={this.state.loaded ? 'tower' : 'tower hidden'} style={{}}>
-        <div className="disc"></div>
-        <div className="disc"></div>
+        {this.generateDiscs()}
       </ul>
     )
   }
---
+++ src/util/general_util.js
@@ -0,0 +1,7 @@
+export const createArrayOfLength = (n) => {
+  let output = [];
+  for (let i = 0; i < n; i++) {
+    output.push(i);
+  }
+  return output;
+}
