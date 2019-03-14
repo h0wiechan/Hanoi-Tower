05b0d0660dd015509e063991040a1bb58ef8a494
h0wiechan <howiechanhy@outlook.com>
2019-03-14 14:58:11 -0400

    Created startGame() which provides each tower the number of discs to start with

--- src/components/game.js
+++ src/components/game.js
@@ -3,23 +3,28 @@ import Tower from './tower';
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    this.noOfDiscs = 3;
     this.state = {
-        loaded: false,
+      loaded: false,
     }
   }
 
   componentDidMount() {
     setTimeout(() => this.setState({loaded: true}), this.props.delay);
   }
+
+  startGame() {
+    return (
+      [0, 1, 2].map((n) => (
+        <Tower idx={n} delay={this.props.delay + 500} noOfDiscs={n === 0 ? this.noOfDiscs : 0}/>
+      ))
+    );
+  }
     
   render() {
     return (
       <div id="playground" className={this.state.loaded ? '' : 'hidden'}>
-        {
-          [0, 1, 2].map((n) => (
-            <Tower idx={n} delay={this.props.delay + 500}/>
-          ))
-        }
+        {this.startGame()}
       </div>
     );
   }
