ef8f9551b6574a0fec4984f5060e4da7c7fe9706
h0wiechan <howiechanhy@outlook.com>
2019-03-14 01:49:12 -0400

    Initial commit with dependencies installed and reset.css set up

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../stylesheets/reset.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          Tower of Hanoi
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
