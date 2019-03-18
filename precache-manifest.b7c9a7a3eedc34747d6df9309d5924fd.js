cade14f0f6ce13a44d16e39155aeb7f55b23a94d
h0wiechan <howiechanhy@outlook.com>
2019-03-18 13:52:32 -0400

    Updates

---
+++ precache-manifest.b7c9a7a3eedc34747d6df9309d5924fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e0373c2b4410c49439dfd822c5fd16e",
+    "url": "/h0wiechan.github.io/Hanoi-Tower/static/media/loader.9e0373c2.gif"
+  },
+  {
+    "revision": "e7aec5e16b6039ef1142e176904a2085",
+    "url": "/h0wiechan.github.io/Hanoi-Tower/static/media/ToT.e7aec5e1.svg"
+  },
+  {
+    "revision": "54af5ee80712cd25be93",
+    "url": "/h0wiechan.github.io/Hanoi-Tower/static/js/runtime~main.7017d207.js"
+  },
+  {
+    "revision": "b28792286013afeb898f",
+    "url": "/h0wiechan.github.io/Hanoi-Tower/static/js/main.ad475729.chunk.js"
+  },
+  {
+    "revision": "3d2b9e3eb628da3cfcce",
+    "url": "/h0wiechan.github.io/Hanoi-Tower/static/js/2.6b81ae53.chunk.js"
+  },
+  {
+    "revision": "b28792286013afeb898f",
+    "url": "/h0wiechan.github.io/Hanoi-Tower/static/css/main.b7d25da8.chunk.css"
+  },
+  {
+    "revision": "70bab1bc4d214e87c749f2dfb85e6b83",
+    "url": "/h0wiechan.github.io/Hanoi-Tower/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/h0wiechan.com/Hanoi-Tower/precache-manifest.887116fbac2007abf9a090f3d3484920.js"
+  "/h0wiechan.github.io/Hanoi-Tower/precache-manifest.b7c9a7a3eedc34747d6df9309d5924fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/h0wiechan.com/Hanoi-Tower/index.html", {
+workbox.routing.registerNavigationRoute("/h0wiechan.github.io/Hanoi-Tower/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
